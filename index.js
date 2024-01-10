0308ab952dbbc7d7f66bfcf729bff20eb64c9d28
cristian german <cristiangerman1998@gmail.com>
2024-01-10 11:42:42 -0300

    nueva version

--- index.js
+++ index.js
@@ -32,75 +32,52 @@ $inputBusqueda.addEventListener("input", (evento) => {
 
 const generosPeliculas = document.getElementById('generos')
 
+// generosPeliculas.addEventListener('change', () => {
+//     const generosLista = generosPeliculas.value;
+//     const filtrar = [];
+//     for (let movie of Movies) {
+//         if (movie.genres.includes(generosLista)) {
+//             filtrar.push(movie);
+//             $peliculas.innerHTML = llenar(filtrar)
+//         } else if (generosLista == "All") {
+//             $peliculas.innerHTML = llenar(Movies);
+//         }
+//     }
+// }
+// )
+
 generosPeliculas.addEventListener('change', () => {
     const generosLista = generosPeliculas.value;
+    const textoBusqueda = $inputBusqueda.value.toLowerCase();
     const filtrar = [];
+
     for (let movie of Movies) {
-        if (movie.genres.includes(generosLista)) {
+        if ((generosLista === "All" || movie.genres.includes(generosLista)) && movie.title.toLowerCase().includes(textoBusqueda)) {
             filtrar.push(movie);
-            $peliculas.innerHTML = llenar(filtrar)
-        } else if (generosLista == "All") {
-            $peliculas.innerHTML = llenar(Movies);
         }
     }
+
+    $peliculas.innerHTML = llenar(filtrar);
+});
+
+
+let mostrarPeli = Movies;
+
+function actualizarPeliculas() {
+    const nombreABuscar = $inputBusqueda.value.toLowerCase();
+    const seleccionarGenero = generosPeliculas.value;
+
+    let peliculasFiltradas = mostrarPeli.filter(movie => {
+        const cumpleNombre = movie.title.toLowerCase().includes(nombreABuscar);
+        const cumpleGenero = seleccionarGenero === 'All' || movie.genres.includes(generoSeleccionado);
+        return cumpleNombre && cumpleGenero;
+    });
+
+    $peliculas.innerHTML = llenar(peliculasFiltradas);
 }
-)
+
+$inputBusqueda.addEventListener("input", actualizarPeliculas);
+generosPeliculas.addEventListener('change', actualizarPeliculas);
 
 
-
-
-
-
-
-
-// // console.log(filterName(Movies,"The Nun II"));
-
-
-
-
-// function filterName(nombrePelicula, nombreMovies) {
-//     return nombrePelicula.filter(movie => movie.title.toLowerCase().includes(nombreMovies.toLowerCase()))
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// crear una funcion que sume 2 numeros 
-// recibidos por parametros y retorne el resultado
-
-// usando la funcion creada anteriormente sumar primero 30 + 15
-// y luego al resultado sumarle 100
-
-// crear un bucle que muestre por consola los numeros del 1 al 5
-
-// dado el array 
-// let numeros = [1,5,10,15]
-
-// crear un nuevo array -> [10,50,100,150] con un metodo de array
-
-
-const numero1 = 30
-const numero2 = 15
-function suma(a, b) {
-    return a + b
-}
-(suma(numero1, numero2));
-
-const nuevoValor = suma(numero1, numero2)
-
+actualizarPeliculas();
